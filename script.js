22864f601f9b85a8f944dd71834e9335c1d22303
Glitch (absorbing-furtive-fukuiraptor) <none>
2020-05-12 20:22:48 +0000

    🍽📙 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,24 @@
+let searchButton = document.querySelector("#search")
+
+//Add an event listener to the button that runs the function sendApiRequest when it is clicked
+searchButton.addEventListener("click", ()=>{
+  console.log("button pressed")
+  sendApiRequest()
+})
+
+
+//An asynchronous function to fetch data from the API.
+async function sendApiRequest(){
+  let response = await fetch(`https://api.lyrics.ovh/v1/Beyonce/Halo`);
+  console.log(response)
+  let data = await response.json()
+  console.log(data)
+  useApiData(data)
+}
+
+
+//function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
+function useApiData(data){
+document.querySelector("#content").innerHTML = data.lyrics
+}
+
